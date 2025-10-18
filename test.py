d4ae853032de73edd3935127af44addc0a589789
Brice Woodburn <bjw6356@rit.edu>
2025-10-17 22:32:04 -0400

    deleted the test script

--- test.py
+++ test.py
@@ -1,11 +1,3 @@
-import mysql.connector
-import pandas as pd
-import matplotlib.pyplot as plt
-from sklearn.ensemble import RandomForestClassifier
-from supabase import create_client, Client
-
-print("✅ All libraries installed successfully!")
-
 # to launch the virtual environment -> venv\Scripts\activate
 # to get into the correct pyton interpreter -> Ctrl+Shift+P then select Python: Select Interpreter
 # to launch fastapi server -> uvicorn app:app --reload
